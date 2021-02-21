41df26959ff8702cc538cf1a7d772fc061f34e81
inna <marmaxkat@gmail.com>
2021-02-21 14:42:22 -0800

    HW_Algorithms_2 added

---
+++ lesson_3/HW_Algorithms_2/fibonacci_for_one_number.py
@@ -0,0 +1,15 @@
+number = int(input('Input first number: '))
+
+
+def Fibonacci(n):
+    if n < 0:
+        print('Not a valid value')
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return Fibonacci(n - 1) + Fibonacci(n - 2)
+
+
+print(Fibonacci(number))
---
+++ lesson_3/HW_Algorithms_2/remove_zeros.py
@@ -0,0 +1,21 @@
+# Numbers ending with zeros are boring. They might be fun in your world, but not here.
+# Get rid of them. Only the ending ones.
+
+# 1450 -> 145
+# 960000 -> 96
+# 1050 -> 105
+# -1050 -> -105
+# Zero alone is fine, don't worry about it. Poor guy anyway
+
+
+
+def remove_zeros(number):
+    while number % 10 == 0:
+        number //= 10
+    return number
+
+
+print(remove_zeros(1450))
+print(remove_zeros(960000))
+print(remove_zeros(1050))
+print(remove_zeros(-1050))
