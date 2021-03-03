d5bb2f28ff70193b882a537390a4502d7a68f814
inna <marmaxkat@gmail.com>
2021-03-03 13:35:19 -0800

    Recursion for Factorial test added

--- lesson_3/HW_Algorithms_3/Factorial_rec.py
+++ lesson_3/HW_Algorithms_3/Factorial_rec.py
@@ -1,7 +1,9 @@
 n = int(input('Input number: '))
 
 def factorial_rec(number):
-
-    return 1 if (number==0 or number==1) else number * factorial_rec(number-1)
+    if number < 0:
+        return 'I don\'t like to work with Gamma Function'
+    else:
+        return 1 if (number == 0 or number == 1) else number * factorial_rec(number - 1)
 
 print(factorial_rec(n))
