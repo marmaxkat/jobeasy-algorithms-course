1298c2068e789a71675059988bd75a2090e2388f
inna <marmaxkat@gmail.com>
2021-03-04 11:51:50 -0800

    Recursion for Digital root test added

---
+++ lesson_3/HW_Algorithms_3/digital_root_rec.py
@@ -0,0 +1,12 @@
+def digital_root_rec(num):
+    if len(str(num)) == 1:
+        return num
+    sum = 0
+    for i in str(num):
+        sum += int(i)
+    return digital_root_rec(sum)
+
+print(digital_root_rec(16))
+print(digital_root_rec(942))
+print(digital_root_rec(132189))
+print(digital_root_rec(493193))
