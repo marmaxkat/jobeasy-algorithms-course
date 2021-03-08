57374b823dd233b5a23dd642b09ce2c4d7cf7260
inna <marmaxkat@gmail.com>
2021-03-08 14:07:44 -0800

    import get_divisors added to perfect_number

--- lesson_3/HW_Algorithms_3/perfect_number.py
+++ lesson_3/HW_Algorithms_3/perfect_number.py
@@ -1,17 +1,25 @@
-number = int(input('Enter number: '))
+# number = int(input('Enter number: '))
+#
+#
+# def is_perfect(n):
+#     perfect_sum = 0
+#
+#     for i in range(1, n):
+#         if n % i == 0:
+#             perfect_sum += i
+#
+#     return perfect_sum == n
+#
+#
+# if is_perfect(number):
+#     print('%d is PERFECT' %(number))
+# else:
+#     print('%d is NOT PERFECT' %(number))
+
+from lesson_3.divisors import get_divisors
+
+def is_perfect(num):
+    return num == sum(get_divisors(num, False))
 
 
-def is_perfect(n):
-    perfect_sum = 0
-
-    for i in range(1, n):
-        if n % i == 0:
-            perfect_sum += i
-
-    return perfect_sum == n
-
-
-if is_perfect(number):
-    print('%d is PERFECT' %(number))
-else:
-    print('%d is NOT PERFECT' %(number))
+print(is_perfect(29))
